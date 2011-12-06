ff0907690fbd9846b73e3945fa1e3c39d33df90c
Andrew Brobston <andrew@brobston.com>
2011-12-06 00:42:14 -0600

    Fixed bug when updating with no fields changed.

--- runtime/lib/util/BasePeer.php
+++ runtime/lib/util/BasePeer.php
@@ -340,6 +340,16 @@ class BasePeer
 		// we also need the columns for the update SQL
 		$updateTablesColumns = $updateValues->getTablesColumns();
 
+		// If no columns are changing values, we may get here with
+		// an empty array in $updateTablesColumns.  In that case,
+		// there is nothing to do, so we return the rows affected,
+		// which is 0.  Fixes a bug in which an UPDATE statement
+		// would fail in this instance.
+
+		if (empty($updateTablesColumns)) {
+			return 0;
+		}
+
 		$affectedRows = 0; // initialize this in case the next loop has no iterations.
 
 		foreach ($tablesColumns as $tableName => $columns) {
