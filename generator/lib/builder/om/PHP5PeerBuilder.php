propelorm/Propel

commit 5ce2674de7a1a255845e3d23cf6ed48417c9cd26

Allowed FQCN with namespaces in inheritance keys (working solution). Fixes #135

generator/lib/builder/om/PHP5PeerBuilder.php
@@ -537,6 +537,7 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 				foreach ($col->getChildren() as $child) {
 					$childBuilder = $this->getMultiExtendObjectBuilder();
 					$childBuilder->setChild($child);
+					$fqcn = addslashes('\\'.$childBuilder->getFullyQualifiedClassname());
 
 					$script .= "
 	/** A key representing a particular subclass */
@@ -546,13 +547,13 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 					if (strtoupper($child->getClassname()) != strtoupper($child->getKey())) {
 						$script .= "
 	/** A key representing a particular subclass */
-	const CLASSKEY_".strtoupper($child->getClassname())." = '" . $child->getKey() . "';
+	const CLASSKEY_".strtoupper($child->getClassname())." = '" . $fqcn . "';
 ";
 					}
 
 					$script .= "
 	/** A class that can be returned by this peer. */
-	const CLASSNAME_".strtoupper($child->getKey())." = '". $childBuilder->getClasspath() . "';
+	const CLASSNAME_".strtoupper($child->getKey())." = '". $fqcn . "';
 ";
 				} /* foreach children */
 			} /* if col->isenumerated...() */

generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -3436,6 +3436,8 @@ abstract class ".$this->getClassname()." extends ".$parentClass." ";
 
 		if ($tblFK->getChildrenColumn()) {
 			$className = 'Base' . $className;
+            $namespace = $joinedTableObjectBuilder->getNamespace();
+            $this->declareClass($namespace.'\\'.$className);
 		}
 
 		$collName = $this->getRefFKCollVarName($refFK);

commit 9d8e12af4898a907920722e4b5af81224b5aa26d

fixes #135
Error and BD break was introduced in 5ce2674de7a1a255845e3d23cf6ed48417c9cd26
this commit fixes it

generator/lib/builder/om/PHP5PeerBuilder.php
@@ -537,7 +537,7 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 				foreach ($col->getChildren() as $child) {
 					$childBuilder = $this->getMultiExtendObjectBuilder();
 					$childBuilder->setChild($child);
-					$fqcn = addslashes('\\'.$childBuilder->getFullyQualifiedClassname());
+					$fqcn = addslashes($childBuilder->getFullyQualifiedClassname());
 
 					$script .= "
 	/** A key representing a particular subclass */
@@ -547,7 +547,7 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
 					if (strtoupper($child->getClassname()) != strtoupper($child->getKey())) {
 						$script .= "
 	/** A key representing a particular subclass */
-	const CLASSKEY_".strtoupper($child->getClassname())." = '" . $fqcn . "';
+	const CLASSKEY_".strtoupper($child->getClassname())." = '" . $child->getKey() . "';
 ";
 					}
 