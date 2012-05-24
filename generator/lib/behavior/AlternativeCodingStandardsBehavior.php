1c5c3784dab7447f07a897212ab465840d7fd936
William DURAND <william.durand1@gmail.com>
2012-05-24 14:03:01 +0200

    Fixed bad indentation

--- generator/lib/behavior/AlternativeCodingStandardsBehavior.php
+++ generator/lib/behavior/AlternativeCodingStandardsBehavior.php
@@ -39,13 +39,13 @@
 class AlternativeCodingStandardsBehavior extends Behavior
 {
     // default parameters value
-  protected $parameters = array(
-      'brackets_newline'        => 'true',
-      'remove_closing_comments' => 'true',
-      'use_whitespace'          => 'true',
-      'tab_size'                => 2,
-      'strip_comments'          => 'false'
-  );
+	protected $parameters = array(
+		'brackets_newline'        => 'true',
+		'remove_closing_comments' => 'true',
+		'use_whitespace'          => 'true',
+		'tab_size'                => 2,
+		'strip_comments'          => 'false'
+	);
 
     public function objectFilter(&$script)
     {
