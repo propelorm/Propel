propelorm/Propel

commit 841d477db7e30c0066860ccda580217cd7f9204b

add @throws and @return where missing
* replace some @deprecated method calls
* add return statements where missing
* fix @param type hints

generator/lib/builder/om/PHP5PeerBuilder.php
@@ -789,6 +789,8 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
      * Adds the PHP code to return a instance pool key for the passed-in primary key variable names.
      *
      * @param array $pkphp An array of PHP var names / method calls representing complete pk.
+     *
+     * @return string
      */
     public function getInstancePoolKeySnippet($pkphp)
     {
@@ -1841,7 +1843,7 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
             }
 
             foreach (\$cols as \$colName) {
-                if (\$tableMap->containsColumn(\$colName)) {
+                if (\$tableMap->hasColumn(\$colName)) {
                     \$get = 'get' . \$tableMap->getColumn(\$colName)->getPhpName();
                     \$columns[\$colName] = \$obj->\$get();
                 }

runtime/lib/map/TableMap.php
@@ -317,7 +317,7 @@ class TableMap
      */
     public function addConfiguredColumn($cmap)
     {
-        $this->columns[ $cmap->getColumnName() ] = $cmap;
+        $this->columns[ $cmap->getName() ] = $cmap;
 
         return $cmap;
     }
@@ -646,7 +646,7 @@ class TableMap
     /**
      * Gets the ColumnMap for the primary string column.
      *
-     * @return ColumnMap
+     * @return ColumnMap|bool The primary key column, false if none given.
      */
     public function getPrimaryStringColumn()
     {
@@ -655,6 +655,8 @@ class TableMap
                 return $column;
             }
         }
+
+        return false;
     }
 
     // Deprecated methods and attributres, to be removed

runtime/lib/query/Criteria.php
@@ -246,7 +246,7 @@ class Criteria implements IteratorAggregate
      * Creates a new instance with the default capacity which corresponds to
      * the specified database.
      *
-     * @param      dbName The dabase name.
+     * @param string $dbName The dabase name.
      */
     public function __construct($dbName = null)
     {
@@ -339,13 +339,15 @@ class Criteria implements IteratorAggregate
      * Returns the column name associated with an alias (AS-column).
      *
      * @param  string $alias
-     * @return string $string
+     * @return string|bool $string The name if found, false otherwise.
      */
     public function getColumnForAs($as)
     {
         if (isset($this->asColumns[$as])) {
             return $this->asColumns[$as];
         }
+
+        return false;
     }
 
     /**
@@ -392,13 +394,15 @@ class Criteria implements IteratorAggregate
      * Returns the table name associated with an alias.
      *
      * @param  string $alias
-     * @return string $string
+     * @return string|false $string The name if given, false otherwise.
      */
     public function getTableForAlias($alias)
     {
         if (isset($this->aliases[$alias])) {
             return $this->aliases[$alias];
         }
+
+        return false;
     }
 
     /**
@@ -796,6 +800,10 @@ class Criteria implements IteratorAggregate
      * @param array  $criterions array of the name of the criterions to combine
      * @param string $operator   logical operator, either Criteria::LOGICAL_AND, or Criteria::LOGICAL_OR
      * @param string $name       optional name to combine the criterion later
+     *
+     * @return Criteria
+     *
+     * @throws PropelException
      */
     public function combine($criterions = array(), $operator = self::LOGICAL_AND, $name = null)
     {
@@ -1384,7 +1392,7 @@ class Criteria implements IteratorAggregate
      * Remove an object from the criteria.
      *
      * @param  string $key A string with the key to be removed.
-     * @return mixed  The removed value.
+     * @return mixed|null  The removed value, null if not set.
      */
     public function remove($key)
     {
@@ -1397,6 +1405,8 @@ class Criteria implements IteratorAggregate
 
             return $removed;
         }
+
+        return null;
     }
 
     /**
@@ -1506,6 +1516,8 @@ class Criteria implements IteratorAggregate
      *            This parameter is deprecated, use _or() instead
      *
      * @return Criteria The current criteria object
+     *
+     * @throws PropelException
      */
     public function mergeWith(Criteria $criteria, $operator = null)
     {
@@ -1636,7 +1648,7 @@ class Criteria implements IteratorAggregate
         } elseif (is_int($comparison)) {
             // $comparison is a PDO::PARAM_* constant value
             // something like $c->add('foo like ?', '%bar%', PDO::PARAM_STR);
-            return new Criterion($this, $p1, $value, Criteria::RAW, $comparison);;
+            return new Criterion($this, $p1, $value, Criteria::RAW, $comparison);
         } else {
             // $comparison is one of Criteria's constants
             // something like $c->add(BookPeer::TITLE, 'War%', Criteria::LIKE);
@@ -1779,6 +1791,8 @@ class Criteria implements IteratorAggregate
      * @param bool $cond ignored
      *
      * @return PropelConditionalProxy|Criteria
+     *
+     * @throws PropelException
      */
     public function _elseif($cond)
     {
@@ -1794,6 +1808,8 @@ class Criteria implements IteratorAggregate
      * Allows for conditional statements in a fluid interface.
      *
      * @return PropelConditionalProxy|Criteria
+     *
+     * @throws PropelException
      */
     public function _else()
     {
@@ -1809,6 +1825,8 @@ class Criteria implements IteratorAggregate
      * Allows for conditional statements in a fluid interface.
      *
      * @return Criteria
+     *
+     * @throws PropelException
      */
     public function _endif()
     {

generator/lib/builder/om/OMBuilder.php
@@ -295,6 +295,8 @@ abstract class OMBuilder extends DataModelBuilder
      * @param string $classname The Peer classname to use.
      *
      * @return string If $classname is provided, then will return $classname::COLUMN_NAME; if not, then the peername is looked up for current table to yield $currTablePeer::COLUMN_NAME.
+     *
+     * @throws Exception
      */
     public function getColumnConstant($col, $classname = null)
     {
@@ -397,6 +399,8 @@ abstract class OMBuilder extends DataModelBuilder
      * will be appended.
      *
      * @return string
+     *
+     * @throws Exception
      */
     protected static function getRelatedBySuffix(ForeignKey $fk)
     {

generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -130,6 +130,7 @@ class PHP5ObjectBuilder extends ObjectBuilder
      * Returns the type-casted and stringified default value for the specified Column.
      * This only works for scalar default values currently.
      * @return string The default value or 'NULL' if there is none.
+     * @throws EngineException
      */
     protected function getDefaultValueString(Column $col)
     {

generator/lib/builder/om/PHP5ObjectNoCollectionBuilder.php
@@ -555,6 +555,7 @@ class PHP5ObjectNoCollectionBuilder extends PHP5ObjectBuilder
     /**
      * Adds the method that fetches fkey-related (referencing) objects but also joins in data from another table.
      * @param      string &$script The script will be modified in this method.
+     * @throws EngineException
      */
     protected function addRefFKGetJoinMethods(&$script, ForeignKey $refFK)
     {

generator/lib/builder/util/PropelTemplate.php
@@ -61,6 +61,9 @@ class PropelTemplate
      * @param array $vars An associative array of argumens to be rendered
      *
      * @return string The rendered template
+     *
+     * @throws InvalidArgumentException
+     * @throws Exception
      */
     public function render($vars = array())
     {

generator/lib/builder/util/XmlToAppData.php
@@ -101,6 +101,7 @@ class XmlToAppData
      * @param  string  $xmlString The input string to parse.
      * @param  string  $xmlFile   The input file name.
      * @return AppData populated by <code>xmlFile</code>.
+     * @throws Exception
      */
     public function parseString($xmlString, $xmlFile = null)
     {
@@ -138,6 +139,8 @@ class XmlToAppData
      * @param string $rawName The qualified name (with prefix), or the empty string if
      *		 qualified names are not available.
      * @param string $attributes The specified or defaulted attributes
+     *
+     * @throws SchemaException
      */
     public function startElement($parser, $name, $attributes)
     {

commit 62fd2bf85a321cbb63fb7659230e784fc4110a94

fix regressions introduced in 841d477db7

generator/lib/builder/om/PHP5PeerBuilder.php
@@ -462,6 +462,7 @@ abstract class ".$this->getClassname(). $extendingPeerClass . " {
     protected function addGetValueSets(&$script)
     {
         $this->declareClassFromBuilder($this->getTableMapBuilder());
+        $callingClass = $this->getStubPeerBuilder()->getClassname();
         $script .= "
     /**
      * Gets the list of values for all ENUM columns

runtime/lib/map/TableMap.php
@@ -646,7 +646,7 @@ class TableMap
     /**
      * Gets the ColumnMap for the primary string column.
      *
-     * @return ColumnMap|bool The primary key column, false if none given.
+     * @return ColumnMap|null The primary string column, null if none given.
      */
     public function getPrimaryStringColumn()
     {
@@ -656,7 +656,7 @@ class TableMap
             }
         }
 
-        return false;
+        return null;
     }
 
     // Deprecated methods and attributres, to be removed

runtime/lib/query/Criteria.php
@@ -339,7 +339,7 @@ class Criteria implements IteratorAggregate
      * Returns the column name associated with an alias (AS-column).
      *
      * @param  string $alias
-     * @return string|bool $string The name if found, false otherwise.
+     * @return string|null $string The name if found, null otherwise.
      */
     public function getColumnForAs($as)
     {
@@ -347,7 +347,7 @@ class Criteria implements IteratorAggregate
             return $this->asColumns[$as];
         }
 
-        return false;
+        return null;
     }
 
     /**
@@ -394,7 +394,7 @@ class Criteria implements IteratorAggregate
      * Returns the table name associated with an alias.
      *
      * @param  string $alias
-     * @return string|false $string The name if given, false otherwise.
+     * @return string|null $string The name if given, null otherwise.
      */
     public function getTableForAlias($alias)
     {
@@ -402,7 +402,7 @@ class Criteria implements IteratorAggregate
             return $this->aliases[$alias];
         }
 
-        return false;
+        return null;
     }
 
     /**