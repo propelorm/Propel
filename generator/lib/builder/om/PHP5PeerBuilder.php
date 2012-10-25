914d5fc9fcfa7a8a95b5955c8a3762359bb513bc
Chris <andrea.cristaudo@gmail.com>
2012-10-25 13:03:20 +0300

    Fixed serializer for datetime primary keys

--- generator/lib/builder/om/PHP5PeerBuilder.php
+++ generator/lib/builder/om/PHP5PeerBuilder.php
@@ -806,11 +806,11 @@ abstract class ".$this->getClassname(). $extendingPeerClass . "
             $script .= "serialize(array(";
             $i = 0;
             foreach ($pkphp as $pkvar) {
-                $script .= ($i++ ? ', ' : '') . "(string) ((" . $pkvar . " instanceof \DateTime) ? " . $pkvar . "->format('U') : " . $pkvar . ")";
+                $script .= ($i++ ? ', ' : '') . "(string) ((" . $pkvar . " instanceof \DateTime) ? " . $pkvar . "->format('r') : " . $pkvar . ")";
             }
             $script .= "))";
         } else {
-            $script .= ($i++ ? ', ' : '') . "(string) ((" . $pkphp[0] . " instanceof \DateTime) ? " . $pkphp[0] . "->format('U') : " . $pkphp[0] . ")";
+            $script .= ($i++ ? ', ' : '') . "(string) ((" . $pkphp[0] . " instanceof \DateTime) ? " . $pkphp[0] . "->format('r') : " . $pkphp[0] . ")";
         }
 
         return $script;
