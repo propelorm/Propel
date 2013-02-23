3c53b826e1070673b6e56330e9f6f2beebdb8016
MArcJ <marc@kryn.org>
2013-02-24 00:58:46 +0100

    Fixed #617. Proved the issue #617 and fixed it.
    Adds now always the `Engine` vendor information in `MysqlSchemaParser` no matter if the build property `addVendorInfo` is true or not, because we need it in `MysqlPlatform` to check whether the table is `InnoDB` and thus with foreignKey support.
    If `addVendorInfo` is true, it extracts **all** vendor information (as before).
    Added a test that proves the bug and a helper class for more tests like that.

--- generator/lib/reverse/mysql/MysqlSchemaParser.php
+++ generator/lib/reverse/mysql/MysqlSchemaParser.php
@@ -140,9 +140,7 @@ class MysqlSchemaParser extends BaseSchemaParser
             $this->addIndexes($table);
             $this->addPrimaryKey($table);
 
-            if ($this->addVendorInfo) {
-                $this->addTableVendorInfo($table);
-            }
+            $this->addTableVendorInfo($table);
         }
 
         return count($tables);
@@ -419,6 +417,10 @@ class MysqlSchemaParser extends BaseSchemaParser
     {
         $stmt = $this->dbh->query("SHOW TABLE STATUS LIKE '" . $table->getName() . "'");
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$this->addVendorInfo) {
+            //since we depend on `Engine` in the MysqlPlatform, we have always extract this vendor information
+            $row = array('Engine' => $row['Engine']);
+        }
         $vi = $this->getNewVendorInfoObject($row);
         $table->addVendorInfo($vi);
     }
