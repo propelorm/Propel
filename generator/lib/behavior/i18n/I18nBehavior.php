926d70a6464c927b05a15f5aac353e0f07a48ec0
Charles SANQUER <charles.sanquer@spyrit.net>
2013-04-08 10:57:56 +0200

    add locale length option for i18n behavior

--- generator/lib/behavior/i18n/I18nBehavior.php
+++ generator/lib/behavior/i18n/I18nBehavior.php
@@ -30,6 +30,7 @@ class I18nBehavior extends Behavior
         'i18n_columns'  => '',
         'i18n_pk_name'  => null,
         'locale_column' => 'locale',
+        'locale_length' => 5,
         'default_locale' => null,
         'locale_alias'  => '',
     );
@@ -128,7 +129,7 @@ class I18nBehavior extends Behavior
             $this->i18nTable->addColumn(array(
                 'name'       => $localeColumnName,
                 'type'       => PropelTypes::VARCHAR,
-                'size'       => 5,
+                'size'       => $this->getParameter('locale_length') ? (int) $this->getParameter('locale_length') : 5,
                 'default'    => $this->getDefaultLocale(),
                 'primaryKey' => 'true',
             ));
