ff72d7531e4c2938d3e4239eeab2c29fb34db54e
enigmatiqk <enigmatiqk@hotmail.com>
2013-10-11 15:00:02 +0200

    Update PHP5ObjectBuilder.php

--- generator/lib/builder/om/PHP5ObjectBuilder.php
+++ generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -1587,8 +1587,19 @@ abstract class ".$this->getClassname()." extends ".$parentClass." ";
 
         $fmt = var_export($this->getTemporalFormatter($col), true);
 
+        // Default date/time formatter strings are specified in build.properties
+        if ($col->getType() === PropelTypes::DATE) {
+         $defaultfmt = $this->getBuildProperty('defaultDateFormat');
+        } elseif ($col->getType() === PropelTypes::TIME) {
+          $defaultfmt = $this->getBuildProperty('defaultTimeFormat');
+        } elseif ($col->getType() === PropelTypes::TIMESTAMP) {
+          $defaultfmt = $this->getBuildProperty('defaultTimeStampFormat');
+        }
+    
+        if (empty($defaultfmt)) { $defaultfmt = null; }
+    
         $script .= "
-        \$dt = PropelDateTime::newInstance(\$v, null, '$dateTimeClass');
+        \$dt = PropelDateTime::newInstance( \$v, null, '$dateTimeClass',\$defaultfmt );
         if (\$this->$clo !== null || \$dt !== null) {
             \$currentDateAsString = (\$this->$clo !== null && \$tmpDt = new $dateTimeClass(\$this->$clo)) ? \$tmpDt->format($fmt) : null;
             \$newDateAsString = \$dt ? \$dt->format($fmt) : null;";
