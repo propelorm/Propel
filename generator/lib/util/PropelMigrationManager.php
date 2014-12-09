e01ca5d92a1232b0c7a4fb3b5010d12c08ae9196
Yasar Kunduz <Yasar@ooip.nl>
2014-12-09 14:06:06 +0100

    Fixed generating database diff

--- generator/lib/util/PropelMigrationManager.php
+++ generator/lib/util/PropelMigrationManager.php
@@ -60,9 +60,15 @@ class PropelMigrationManager
     {
         if (!isset($this->pdoConnections[$datasource])) {
             $buildConnection = $this->getConnection($datasource);
-            $buildConnection['dsn'] = str_replace("@DB@", $datasource, $buildConnection['dsn']);
+            $dsn = str_replace("@DB@", $datasource, $buildConnection['dsn']);
 
-            $this->pdoConnections[$datasource] = Propel::initConnection($buildConnection, $datasource);
+            // Set user + password to null if they are empty strings or missing
+            $username = isset($buildConnection['user']) && $buildConnection['user'] ? $buildConnection['user'] : null;
+            $password = isset($buildConnection['password']) && $buildConnection['password'] ? $buildConnection['password'] : null;
+
+            $pdo = new PropelPDO($dsn, $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdoConnections[$datasource] = $pdo;
         }
 
         return $this->pdoConnections[$datasource];
