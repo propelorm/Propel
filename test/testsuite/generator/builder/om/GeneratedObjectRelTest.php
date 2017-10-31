4a638ff4e02e4f0f4f7b5723300c553d18a92692
jmlegait <jean-michael.legait@crossknowledge.com>
2017-10-31 16:47:03 +0100

    Fix test: Replace getMock by createMock

--- test/testsuite/generator/builder/om/GeneratedObjectRelTest.php
+++ test/testsuite/generator/builder/om/GeneratedObjectRelTest.php
@@ -908,7 +908,7 @@ class GeneratedObjectRelTest extends BookstoreEmptyTestBase
 
         BookPeer::clearInstancePool();
 
-        $summary = $this->getMock('BookSummary');
+        $summary = $this->createMock(BookSummary::class);
         $summary
             ->expects($this->once())
             ->method('isDeleted')
