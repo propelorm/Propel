43f5d3a30d8e4c6f3dc341513f852ce0194e6ef4
Mike Petrovich <michael.c.petrovich@gmail.com>
2018-02-17 16:41:18 -0500

    Cast limit to integer when setting via Criteria::setLimit()
    This prevents SQL injection via limit() and is a similar fix as the one added to Propel2 [1]. See that pull request for details.
    
    [1] https://github.com/propelorm/Propel2/pull/1465

--- runtime/lib/query/Criteria.php
+++ runtime/lib/query/Criteria.php
@@ -1239,8 +1239,7 @@ class Criteria implements IteratorAggregate
      */
     public function setLimit($limit)
     {
-        // TODO: do we enforce int here? 32bit issue if we do
-        $this->limit = $limit;
+        $this->limit = (int) $limit;
 
         return $this;
     }
@@ -1258,8 +1257,7 @@ class Criteria implements IteratorAggregate
     /**
      * Set offset.
      *
-     * @param int $offset An int with the value for offset.  (Note this values is
-     *                    cast to a 32bit integer and may result in truncation)
+     * @param int $offset An int with the value for offset.
      *
      * @return Criteria Modified Criteria object (for fluent API)
      */
