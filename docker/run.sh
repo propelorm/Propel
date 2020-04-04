e76bbd1354bc76c993fddf7054fcfb16ea45aba9
Luke Adamczewski <luke.adamczewski@jobleads.de>
2020-04-04 23:50:49 +0200

    Added docker setup

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-cd docker
-docker-compose up -d db
+docker compose up -d 
 sleep 20
-docker-compose exec -T db bash < reset.sh
-docker-compose build
-docker-compose run php
+docker compose run db /bin/bash reset.sh
+docker compose run php
