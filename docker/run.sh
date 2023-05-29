bbc2ce8e7282208961ef0b6c850362b8a45d5a0b
David Goodwin <david@codepoets.co.uk>
2023-05-29 19:29:40 +0100

    update docker image, use php8.1

--- docker/run.sh
+++ docker/run.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
 set -x
-
 id
+pwd
+cd $(dirname $0) 
 
-/bin/bash docker/reset.sh
+env
 
-curl -o composer https://getcomposer.org/download/2.5.1/composer.phar
-chmod 755 composer
 
-./composer install -n
-bash test/reset_tests.sh
+mysql -u root -h mysql -e 'SET FOREIGN_KEY_CHECKS = 0; DROP DATABASE IF EXISTS test; DROP SCHEMA IF EXISTS second_hand_books; DROP SCHEMA IF EXISTS contest; DROP DATABASE IF EXISTS reverse_bookstore; DROP SCHEMA IF EXISTS bookstore_schemas; SET FOREIGN_KEY_CHECKS = 1;'
+mysql -u root -h mysql -e 'CREATE DATABASE test; CREATE SCHEMA bookstore_schemas; CREATE SCHEMA contest; CREATE SCHEMA second_hand_books; CREATE DATABASE reverse_bookstore; CREATE DATABASE bookstore_schemas;'
+
+for file in ../test/fixtures/bookstore/build/sql/*.sql test/fixtures/schemas/build/sql/schema.sql
+do
+    echo "Loading : $file "
+    cat $file | mysql -u root -h mysql test
+done
+
+echo "test reset"
+
+bash -x ../test/reset_tests.sh
